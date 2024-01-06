a3a2e90bb351d360fe7a8baeb73c18a63844d32e
daredevilop <140168998+daredevilop@users.noreply.github.com>
2024-01-06 20:46:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,24 +13,24 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = 18136872
 API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", default="DARK_RAVAN")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", default=None)
-BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
-BOT_TOKEN3 = getenv("BOT_TOKEN3", default=None)
-BOT_TOKEN4 = getenv("BOT_TOKEN4", default=None)
-BOT_TOKEN5 = getenv("BOT_TOKEN5", default=None)
-BOT_TOKEN6 = getenv("BOT_TOKEN6", default=None)
-BOT_TOKEN7 = getenv("BOT_TOKEN7", default=None)
-BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
-BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
-BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
+BOT_TOKEN = getenv("BOT_TOKEN", default="6215534047:AAGC5JvFzG69CaJ9Q6u0vukFrpY5kxvlmm4")
+BOT_TOKEN2 = getenv("BOT_TOKEN2", default="6568638985:AAGLbu9Cb35t-ZkDY0hqn6pSalw5QeHMNjg")
+BOT_TOKEN3 = getenv("BOT_TOKEN3", default="6478217552:AAFEr7aZeesOyv-S821QZB9CkjAi7gyxuf4")
+BOT_TOKEN4 = getenv("BOT_TOKEN4", default="6466925710:AAHI-7RXl2ff80vB6U0p4eb6EerHhPX-BFs")
+BOT_TOKEN5 = getenv("BOT_TOKEN5", default="6518264121:AAF1MWRrFidfXZK9luI7FrG6UGOucW5nHr8")
+BOT_TOKEN6 = getenv("BOT_TOKEN6", default="6459651344:AAEhKIELqkwrhfOdZMdDELhb-LkG8mOmsIQ")
+BOT_TOKEN7 = getenv("BOT_TOKEN7", default="5852275658:AAGLTVIySLjNLmJvWTP0JXpS44wzqYod1VI")
+BOT_TOKEN8 = getenv("BOT_TOKEN8", default="6680841962:AAFKYAMng1PkXGCEJ9OeaBqlXf8JX9gBBg0")
+BOT_TOKEN9 = getenv("BOT_TOKEN9", default="6464222164:AAEnDpXNY-YVfcKFZQ8KrUKAWfcFW4NHOAY")
+BOT_TOKEN10 = getenv("BOT_TOKEN10", default="6461908827:AAHOi9Ftp6DtDjO-gTzPGaF4fief1ZUq25I")
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5695560295").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="6638057092").split()))
 for x in ALTRON:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default=None)
+OWNER_ID = int(getenv("OWNER_ID", default="5695560295")
 SUDO_USERS.append(OWNER_ID)
 
 
